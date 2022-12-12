90e696e04e6a6421e8bb54c7fca778457358f7fd
alan910127 <alan469357@gmail.com>
2022-12-13 00:14:30 +0800

    :technologist: Add env vars for port, api url

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { apiRouter } from "./api/api";
 import { logger } from "./utils/logger";
 
 const app = express();
-const { PORT = 8000 } = process.env;
+const { PORT = 8000, API_URL = "/api" } = process.env;
 
 app.use(express.json());
 
@@ -11,7 +11,7 @@ app.get("/", (_, res) => {
   res.json({ ping: "pong" });
 });
 
-app.use("/api", apiRouter);
+app.use(API_URL, apiRouter);
 
 app.listen(PORT, () => {
   logger.info(`🚀 Server ready at: http://localhost:${PORT}`);
