e22c0ff3b47f1626ed5cacc033f1c837de388d99
alan910127 <alan469357@gmail.com>
2022-12-13 00:23:14 +0800

    :sparkles: Simple template for greet route

--- src/api/routes/greet.ts
+++ src/api/routes/greet.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
+import prisma from "../../lib/prisma";
+import { getUsersByBirthday } from "../../service/greet/getUserByBirthday";
+import { greetSchema } from "../../service/greet/greetSchema";
+import { zParse } from "../../utils/zParse";
 
 export const greetRouter = Router();
 
 greetRouter.get("/", async (req, res) => {
-  res.send("Hi");
+  const {
+    query: { month, day },
+  } = await zParse(greetSchema, req);
+  const users = await getUsersByBirthday({ prisma, month, day });
+  res.status(200).send(users);
 });
