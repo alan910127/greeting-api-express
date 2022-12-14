d04c3f37c0360fdfaa66f726d3076635f4f0099d
alan910127 <alan469357@gmail.com>
2022-12-14 19:45:19 +0800

    :memo: Update swagger ui docs

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,36 +1,65 @@
 import SwaggerAutogen from "swagger-autogen";
 
-const swagger = SwaggerAutogen();
+const swagger = SwaggerAutogen({ openapi: "3.0.0" });
 
 const docs = {
   info: {
-    version: "0.1.0",
-    title: "Greeting Server",
-    description: "Greet all members whose birthday is today",
+    version: "0.1.0", // by default: '1.0.0'
+    title: "Greeting API Server", // by default: 'REST API'
+    description: "", // by default: ''
   },
-  host: "localhost:8000",
-  tags: [{ name: "default" }, { name: "Greet" }, { name: "Users" }],
+  servers: [
+    {
+      url: "http://localhost:8000/",
+    },
+  ],
+  consumes: ["application/json"], // by default: ['application/json']
+  produces: ["application/json"], // by default: ['application/json']
+  tags: [
+    // by default: empty Array
+    { name: "default" },
+    { name: "Greet" },
+    { name: "Users" },
+  ],
+  securityDefinitions: {}, // by default: empty object
   definitions: {
-    GreetResponse: "<Greet Message>",
+    Greet: [
+      {
+        id: "clbl1m9d20000976xz5frzady",
+        firstName: "John",
+        lastName: "Doe",
+        gender: "MALE",
+        dateOfBirth: "2000-01-01:00:00.000Z",
+        email: "john.doe@linecorp.com",
+      },
+    ],
     User: {
       id: "clbl1m9d20000976xz5frzady",
       firstName: "John",
       lastName: "Doe",
       gender: "MALE",
-      dateOfBirth: "1987-06-05T00:00:00.000Z",
-      email: "johndoe@example.com",
+      dateOfBirth: "2000-01-01:00:00.000Z",
+      email: "john.doe@linecorp.com",
     },
-    UserWithoutId: {
+    UserCreate: {
+      $firstName: "John",
+      $lastName: "Doe",
+      $gender: "MALE",
+      $dateOfBirth: "2000-01-01:00:00.000Z",
+      $email: "john.doe@linecorp.com",
+    },
+    UserUpdate: {
       firstName: "John",
       lastName: "Doe",
       gender: "MALE",
-      dateOfBirth: "1987-06-05T00:00:00.000Z",
-      email: "johndoe@example.com",
+      dateOfBirth: "2000-01-01:00:00.000Z",
+      email: "john.doe@linecorp.com",
     },
     UserNotFound: {
-      message: "User not found",
+      message: "User Not Found",
     },
-  },
+  }, // by default: empty object (Swagger 2.0)
+  components: {}, // by default: empty object (OpenAPI 3.x)
 };
 
 const outputFile = "./openapi.json";
